8770016b972dd7b0cb3e3200866a02800dc92462
Krypt <irwenyap2001@gmail.com>
2020-02-16 00:48:51 +0800

    Clear

--- Engine/Line.cpp
+++
@@ -1,6 +0,0 @@
-#include "Line.h"
-
-void Line::Set(const vec2f& start, const vec2f& end) {
-	offset = start;
-	length = end - start;
-}
--- Engine/SpriteAnimationData.cpp
+++
@@ -1,7 +0,0 @@
-#include "SpriteAnimationData.h"
-
-SpriteAnimationData::SpriteAnimationData()
-	: et(0.f)
-	, currentFrame(0)
-	, loop(true) {}
-
