7135b59786cf4b64832c0cfe3c4a9f8e5fd47eca
lim-james <jameslimbj@gmail.com>
2020-04-28 21:41:13 +0800

    Stripped assets

--- Engine/InvertMaterial.cpp
+++
@@ -1,9 +0,0 @@
-#include "InvertMaterial.h"
-
-Material::Invert::Invert() {
-	shader = new Shader("Files/Shaders/standard2D.vert", "Files/Shaders/invert2D.frag");
-	shader->Use();
-	shader->SetInt("tex", 0);
-}
-
-void Material::Invert::SetAttributes() {}
